5d346c0f67ea6868edbae486bc31c495c2187df7
snehal-shelar <snehal.shelar@mindinventory.com>
2026-01-21 11:20:19 +0530

    Formatted the code and added update/delete APIs.

--- firestore/db_config.py
+++ firestore/db_config.py
@@ -2,11 +2,9 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase import firebase
-import dotenv
+from models import settings
 
-# env = load_dotenv()
-
-cred = credentials.Certificate('/home/snehal/Downloads/patients-simulated-data-firebase-adminsdk-fbsvc-3abf6fdb69.json')
-default_app = firebase_admin.initialize_app(cred)
+creds = credentials.Certificate(settings.FIREBASE_CREDENTIALS_PATH)
+default_app = firebase_admin.initialize_app(creds)
 db_client = firestore.client()
-firebase = firebase.FirebaseApplication('https://patients-simulated-data-default-rtdb.firebaseio.com/', None)
+firebase = firebase.FirebaseApplication(settings.FIREBASE_APPLICATION_URL)
--- firestore/main.py
+++ firestore/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
 from models import CreatePatient
 from db_config import db_client
 
@@ -7,44 +7,51 @@ app = FastAPI()
 
 @app.post("/create-patient")
 def create_patients(request: CreatePatient):
-    """ Create patient's records in patient_simulated_data collection. """
+    """Create patient's records in patient_simulated_data collection."""
 
     try:
-        db_client.collection('patient_simulated_data').document('patient').set(
-        request.__dict__
-        )
+        # db_client.collection("patient_simulated_data").document("patient").set(
+        #     request.__dict__
+        # )
+        db_client.collection("patient_simulated_data").add(request.__dict__)
     except Exception as e:
-        raise e 
-    
+        raise e
+
     return True
 
 
-@app.get("/read-patients")
-def read_patients():
-    """ Read the patient's collection. """
+@app.get("/patients")
+def get_patients():
+    """Read the patient's collection."""
 
-    patients = db_client.collection('patient_simulated_data')
+    patients = db_client.collection("patient_simulated_data")
     records = patients.stream()
     response_data = list()
 
     for record in records:
-        print(f'{record.id} => {record.to_dict()}')
         response_data.append({record.id: record.to_dict()})
 
     return response_data
 
 
-@app.patch("/update-patient")
-def update_patients(request: CreatePatient):
-    """ Update the patient's information. """
- 
+@app.patch("/update-patient/{patient_id}")
+def update_patients(request: CreatePatient, patient_id: str):
+    """Update the patient's information."""
+
     try:
-        db_client.collection('patient_simulated_data').document('patient').update(request.__dict__)
+        db_client.collection("patient_simulated_data").document(patient_id).update(
+            request.__dict__
+        )
     except Exception as e:
-        raise e 
+        raise e
 
     return True
 
-# @app.delete("/delete-patient")
-# def delete_patient():
-#     pass
+
+@app.delete("/delete-patient/{patient_id}")
+def delete_patient(patient_id: str):
+    """ Delete record from the patient's collection. """
+    try:
+        db_client.collection("patient_simulated_data").document(patient_id).delete()
+    except Exception as e:
+        raise e
