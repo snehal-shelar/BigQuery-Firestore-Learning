2a62fe3ebd88b9b4949b015e6ee4b5cc9776d3b0
snehal-shelar <snehal.shelar@mindinventory.com>
2026-01-21 11:26:16 +0530

    Added basic CRUD using forestore db.

---
+++ firestore/main.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI
+from models import CreatePatient
+from db_config import db_client
+
+app = FastAPI()
+
+
+@app.post("/create-patient")
+def create_patients(request: CreatePatient):
+    """Create patient's records in patient_simulated_data collection."""
+
+    try:
+        # db_client.collection("patient_simulated_data").document("patient").set(
+        #     request.__dict__
+        # )
+        db_client.collection("patient_simulated_data").add(request.__dict__)
+    except Exception as e:
+        raise e
+
+    return True
+
+
+@app.get("/patients")
+def get_patients():
+    """Read the patient's collection."""
+
+    patients = db_client.collection("patient_simulated_data")
+    records = patients.stream()
+    response_data = list()
+
+    for record in records:
+        response_data.append({record.id: record.to_dict()})
+
+    return response_data
+
+
+@app.patch("/update-patient/{patient_id}")
+def update_patients(request: CreatePatient, patient_id: str):
+    """Update the patient's information."""
+
+    try:
+        db_client.collection("patient_simulated_data").document(patient_id).update(
+            request.__dict__
+        )
+    except Exception as e:
+        raise e
+
+    return True
+
+
+@app.delete("/delete-patient/{patient_id}")
+def delete_patient(patient_id: str):
+    """ Delete record from the patient's collection. """
+    try:
+        db_client.collection("patient_simulated_data").document(patient_id).delete()
+    except Exception as e:
+        raise e
