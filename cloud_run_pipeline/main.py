12d0923562f38427647a5c2cf468d2f3dab84672
snehal-shelar <snehal.shelar@mindinventory.com>
2026-02-05 11:45:25 +0530

    Updated pipeline code.

--- cloud_run_pipeline/main.py
+++ cloud_run_pipeline/main.py
@@ -4,7 +4,7 @@ import os
 import functions_framework
 from google.cloud import bigquery
 
-from .bq_operations.transform import run_transform, claims_transform
+from .bq_operations.transform import run_transform
 
 client = bigquery.Client()
 
@@ -49,23 +49,3 @@ def gcs_to_bigquery_pipeline(cloud_event):
     except Exception as e:
         logging.error(f"Pipeline failed for {file_name}: {str(e)}")
         raise e
-
-
-
-@functions_framework.cloud_event
-def process_claims_matching(cloud_event):
-    # 1. Get GCS file info
-    data = cloud_event.data
-    bucket_name = data["bucket"]
-    file_name = data["name"]
-
-    # Mocking the API payload values (typically these would come from a database or request)
-    encounter_dos = "2024-01-15"
-    encounter_npi = "1000000001"
-    # payload = data.get("metadata", {})
-    # encounter_dos = payload.get("encounter_dos")
-    # encounter_npi = payload.get("encounter_npi")
-
-    # 2. Access the transformed results
-    results = claims_transform(client, encounter_dos, encounter_npi)
-    logging.info(f"Pipeline finished successfully for {file_name}.")
