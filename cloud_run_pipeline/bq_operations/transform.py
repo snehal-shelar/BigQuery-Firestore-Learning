fbbdfafa0ff92a50351adabce5164e60c3401d9b
snehal-shelar <snehal.shelar@mindinventory.com>
2026-02-05 12:57:47 +0530

    Update transform.py

--- cloud_run_pipeline/bq_operations/transform.py
+++ cloud_run_pipeline/bq_operations/transform.py
@@ -10,16 +10,10 @@ def run_transform(client):
     # Open and read the SQL file as a single string
     with open(sql_path, 'r') as file:
         query_string = file.read()
-
-    # Define the parameters
-    # job_config = bigquery.QueryJobConfig(
-    #     query_parameters=[
-    #         bigquery.ScalarQueryParameter("id_to_find", "INTEGER", 1),
-    #     ]
-    # )
+        
     # Pass the string directly to BigQuery
-    print(f"Executing query from {sql_path}...")
+    print(f"Executing query from {sql_path}")
     query_job = client.query(query_string)
 
     results = query_job.result()
-    print(f"Query completed successfully. Job ID: {query_job.job_id}")
+    print(f"Query completed. Job ID: {query_job.job_id}")
